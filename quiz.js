9eaef992f34d046676e4e647bf8099e2f99cb6e1
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2021-08-29 10:02:20 +0530

    quiz structure and quiz query selections

---
+++ quiz.js
@@ -0,0 +1,5 @@
+const quizForm = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-ans-btn");
+const outputEl = document.querySelector("#output");
+
+const correctAnswers = ["90" , "right-angled"]
