060a05cd0259f4d37e0e5bfa271f6efd72f08ecd
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2021-08-30 17:25:20 +0530

    styles added to all the pages and nav added for navigation

--- area.js
+++ area.js
@@ -4,8 +4,12 @@ const submit = document.querySelector("#submit");
 const outputEl = document.querySelector("#output");
 
 function calculateArea(){
+    if(base.value && height.value != ""){
     const area = (Number(base.value)*Number(height.value))/2;
-    outputEl.innerText = "The area of the Triangle is " + area;
+    outputEl.innerHTML = "<p>The area of the Triangle is " + area + "cm<sup>2</sup></p>";
+    } else {
+        outputEl.innerHTML = "please enter the base and height of the triangle";
+    }
 }
 
 submit.addEventListener("click", calculateArea);
--- hypotenuse.js
+++ hypotenuse.js
@@ -3,8 +3,12 @@ const submit = document.querySelector("#submit");
 const outputEl = document.querySelector("#output");
 
 function calculateHypotenuse(){
+    if(side[0].value&&side[1].value != ""){
     const hypotenuse = Math.sqrt((Number(side[0].value)*Number(side[0].value))+(Number(side[1].value)*Number(side[1].value)));
     outputEl.innerText = "The length of Hypotenuse is " + hypotenuse;
+    } else {
+        outputEl.innerText = "Please enter the values of both sides";
+    }
 }
 
 submit.addEventListener("click", calculateHypotenuse);
